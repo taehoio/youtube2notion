17e9eea4c02bd73ebc6f3d8265ee3a4a594f6805
Taeho Kim <taeho@taeho.io>
2021-06-18 21:25:08 +0900

    Add ffmpeg

---
+++ youtube2notion/ffmpeg.py
@@ -0,0 +1,21 @@
+import subprocess
+
+
+class Ffmpeg:
+
+    @classmethod
+    def take_screenshots(cls, input_filename: str, output_dir: str = ''):
+        cmd_args = [
+            'ffmpeg',
+        #'-hide_banner',
+        #'-v',
+        #'quiet',
+        #'-stats'
+            '-i',
+            input_filename,
+            '-vf',
+            'fps=1/1',    # every second. e.g. every minite is 'fps=1/60'
+            output_dir + 'image_%05d.jpeg',
+        ]
+
+        subprocess.call(cmd_args)
