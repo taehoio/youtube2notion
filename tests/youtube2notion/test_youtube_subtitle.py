8c22e640da122f2c4df2ca99680ff0ce3355a7fa
Taeho Kim <taeho@taeho.io>
2021-08-09 16:28:11 +0900

    fix tests

--- tests/youtube2notion/test_youtube_subtitle.py
+++ tests/youtube2notion/test_youtube_subtitle.py
@@ -1,6 +1,6 @@
 import unittest
 from youtube2notion.youtube_subtitle import YoutubeSubtitle
-from youtube_transcript_api._errors import NoTranscriptFound
+from youtube_transcript_api._errors import TranslationLanguageNotAvailable
 
 
 class TestYoutubeSubtitle(unittest.TestCase):
@@ -18,13 +18,18 @@ class TestYoutubeSubtitle(unittest.TestCase):
         self.assertIsNotNone(
             YoutubeSubtitle.get_subtitle_elements(self.video_id))
 
-    def test_get_subtitle_with_specific_language(self):
+    def test_with_manually_created_subtitle(self):
         self.assertIsNotNone(
-            YoutubeSubtitle.get_subtitle_elements(self.video_id, ['ko', 'en']))
-        self.assertIsNotNone(
-            YoutubeSubtitle.get_subtitle_elements(self.video_id,
-                                                  ['aa', 'bb', 'en']))
+            YoutubeSubtitle.get_subtitle_elements('MY5SatbZMAo', ['en']))
 
-    def test_get_subtitle_with_unknown_language_codes_exception(self):
-        with self.assertRaises(NoTranscriptFound):
+    def test_with_auto_generated_subtitle(self):
+        self.assertIsNotNone(
+            YoutubeSubtitle.get_subtitle_elements('5-MXyCr3y5M', ['ko']))
+
+    def test_with_auto_translated_subtitle(self):
+        self.assertIsNotNone(
+            YoutubeSubtitle.get_subtitle_elements('5-MXyCr3y5M', ['en']))
+
+    def test_with_translation_language_not_available_exception(self):
+        with self.assertRaises(TranslationLanguageNotAvailable):
             YoutubeSubtitle.get_subtitle_elements(self.video_id, ['aa', 'bb'])
--- youtube2notion/youtube_subtitle.py
+++ youtube2notion/youtube_subtitle.py
@@ -25,12 +25,17 @@ class SubtitleElement:
 class YoutubeSubtitle:
 
     @classmethod
-    def __fetch_transcript(cls, video_id: str, language_codes: list) -> list:
+    def __fetch_transcript(cls,
+                           video_id: str,
+                           language_codes: list[str] = ['ko', 'en']) -> list:
         """
         Returns a fetched transcript from YouTube. It makes the best effort to
         get a proper transcript following below priorities:
         1. a manually created transcript prior to a auto-generated one.
         2. language codes in language_codes param will be traversed in order.
+        3. if there is neither created nor generated transcript, it will try to
+        get a auto-translated one by the first language code of
+        language_codes param.
         """
         transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
 
@@ -38,14 +43,24 @@ class YoutubeSubtitle:
             return transcript_list.find_manually_created_transcript(
                 language_codes).fetch()
         except NoTranscriptFound:
+            pass
+
+        try:
             return transcript_list.find_generated_transcript(
                 language_codes).fetch()
+        except NoTranscriptFound:
+            pass
+
+        if transcript_list and language_codes:
+            for transcript in transcript_list:
+                return transcript.translate(language_codes[0]).fetch()
 
     @classmethod
     def get_subtitle_elements(
-            cls,
-            video_id: str,
-            language_code_candidates=['ko', 'en']) -> list[SubtitleElement]:
+        cls,
+        video_id: str,
+        language_code_candidates: list[str] = ['ko', 'en']
+    ) -> list[SubtitleElement]:
         fetched_transcript = YoutubeSubtitle.__fetch_transcript(
             video_id, language_code_candidates)
 
