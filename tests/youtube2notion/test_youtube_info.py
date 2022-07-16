f35891b08c435ce996d456846c6d7b85ffa1f8c4
GNOKOHEAT <38800095+gnokoheat@users.noreply.github.com>
2022-07-16 15:35:14 +0900

    refactor for lint, test

--- tests/youtube2notion/test_youtube_info.py
+++ tests/youtube2notion/test_youtube_info.py
@@ -8,6 +8,10 @@ class TestYoutubeInfo(unittest.TestCase):
     def setUp(self):
         self.video_id = '5oNcoj4G0xc'
 
-    def test_get_information_element(self):
+    def test_get_information_element_1(self):
         self.assertIsNotNone(
             YoutubeInfo.get_information_element(self.video_id))
+
+    def test_get_information_element_2(self):
+        self.assertIsNotNone(
+            YoutubeInfo.get_information_element('Kc_cvAXCs4Y'))
--- youtube2notion/youtube_info.py
+++ youtube2notion/youtube_info.py
@@ -3,7 +3,12 @@ import requests
 
 class InformationElement:
 
-    def __init__(self, title: str, author_name: str, author_url: str):
+    def __init__(
+        self,
+        title: str = '',
+        author_name: str = '',
+        author_url: str = '',
+    ):
         self._title = title
         self._author_name = author_name
         self._author_url = author_url
@@ -30,12 +35,15 @@ class YoutubeInfo:
             "format": "json",
             "url": "https://www.youtube.com/watch?v=%s" % video_id
         }
-        res = requests.get(
-            "https://www.youtube.com/oembed", params=params).json()
+        res = requests.get("https://www.youtube.com/oembed", params=params)
 
-        information_element = InformationElement(
-            title=res.get('title'),
-            author_name=res.get('author_name'),
-            author_url=res.get('author_url'),
-        )
-        return information_element
+        if res.status_code == 200:
+            res_json = res.json()
+
+            return InformationElement(
+                title=res_json.get('title'),
+                author_name=res_json.get('author_name'),
+                author_url=res_json.get('author_url'),
+            )
+        else:
+            return InformationElement(title=video_id)
