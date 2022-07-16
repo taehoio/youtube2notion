1e37774139645ca1d3df4d590984b7a8c3ba1a1b
GNOKOHEAT <38800095+gnokoheat@users.noreply.github.com>
2022-07-16 12:17:03 +0900

    Add notion page title

---
+++ tests/youtube2notion/test_youtube_info.py
@@ -0,0 +1,13 @@
+import unittest
+
+from youtube2notion.youtube_info import YoutubeInfo
+
+
+class TestYoutubeInfo(unittest.TestCase):
+
+    def setUp(self):
+        self.video_id = '5oNcoj4G0xc'
+
+    def test_get_information_element(self):
+        self.assertIsNotNone(
+            YoutubeInfo.get_information_element(self.video_id))
---
+++ youtube2notion/youtube_info.py
@@ -0,0 +1,35 @@
+import requests
+
+class InformationElement:
+
+    def __init__(self, title: str, author_name: str, author_url: str):
+        self._title = title
+        self._author_name = author_name
+        self._author_url = author_url
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author_name(self):
+        return self._author_name
+
+    @property
+    def author_url(self):
+        return self._author_url
+
+class YoutubeInfo:
+
+    @classmethod
+    def get_information_element(cls, video_id: str) -> InformationElement:
+
+        params = {"format": "json", "url": "https://www.youtube.com/watch?v=%s" % video_id}
+        res = requests.get("https://www.youtube.com/oembed", params=params).json()
+
+        information_element = InformationElement(
+            title=res.get('title'),
+            author_name=res.get('author_name'),
+            author_url=res.get('author_url'),
+        )
+        return information_element
